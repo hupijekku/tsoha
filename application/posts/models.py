0d520f6069e9a28ad8ef59fa3b95a5f95bbdccbc
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-03-19 20:00:11 +0000

    Commenting code

--- application/posts/models.py
+++ application/posts/models.py
@@ -1,15 +1,23 @@
+# Tuodaan tietokanta
 from application import db
 
+# Luodaan malli blogipostaukselle tietokantaan
 class Post(db.Model):
+    # Primary key (id)
     id = db.Column(db.Integer, primary_key=True)
+    # Viittaus postauksen tehneeseen käyttäjään
     user_id = db.Column(db.Integer)
+    # Päiväys jolloin postaus tehtiin
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+    # Postauksen otsikko ja sisältö
     title = db.Column(db.String(144), nullable=False)
     content = db.Column(db.String(144), nullable=False)
-    # Temporary
+    # Postauksen äänimäärä väliaikaisesti tässä, toteutetaan myöhemmässä vaiheessa
+    # liitostaulun avulla (many-to-many suhde käyttäjään)
     votes = db.Column(db.Integer, default=0)
 
+    # Konstruktori, tallennetaan otsikko ja sisältö
     def __init__(self, title, content):
         self.title = title
         self.content = content
--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
+# Tuodaan Flask ja app
 from flask import render_template
 from application import app
 
+# Default route, renderöidään aloitussivu
 @app.route("/")
 def index():
     return render_template("index.html")
