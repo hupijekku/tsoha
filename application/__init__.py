73a10bbf2676215615f2cacff79a7bc93bd0b9a1
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-03-26 21:40:04 +0200

    Changed init to work with heroku

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,15 @@ app = Flask(__name__)
 # Tuodaan SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
-# Käytetään 'posts.db' tiedostoa databasena. Sijaitsee samassa kansiossa.
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
-# Tulosta kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    # Käytetään 'posts.db' tiedostoa databasena. Sijaitsee samassa kansiossa.
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
+    # Tulosta kaikki SQL-kyselyt
+    app.config["SQLALCHEMY_ECHO"] = True
 
 # Olio jota käytetään tietokannan kyselyihin
 db = SQLAlchemy(app)
@@ -40,4 +45,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan tietokantataulut
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
