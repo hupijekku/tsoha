45685be5708e1bb325c69ddbd610d1d237c1b92b
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-04-03 01:45:04 +0300

    Summary of top posters for front page

--- application/views.py
+++ application/views.py
@@ -1,8 +1,19 @@
 # Tuodaan Flask ja app
 from flask import render_template
-from application import app
+from application import app, db
+from application.auth.models import User
+from application.posts.models import Post, Vote
+
+from sqlalchemy.sql import text
 
 # Default route, renderöidään aloitussivu
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    stmt = text("SELECT account.full_name, COUNT(account.id) as c FROM account LEFT JOIN post ON account.id = post.user_id WHERE post.title IS NOT NULL GROUP BY account.id, post.user_id ORDER BY c DESC")
+    res = db.engine.execute(stmt)
+    response = []
+    for row in res:
+        response.append({"full_name": row[0], "count":row[1]})
+
+    return render_template("index.html", top_posters=response)
