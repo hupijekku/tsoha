b0d03b0d796a4f627a1631402450f34885884057
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-04-02 19:54:43 +0300

    User => Account for postgres

--- application/auth/models.py
+++ application/auth/models.py
@@ -2,7 +2,7 @@ from application import db
 
 class User(db.Model):
 
-    __tablename__ = "user"
+    __tablename__ = "account"
 
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
@@ -10,8 +10,6 @@ class User(db.Model):
     full_name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
-
-    posts = db.relationship("Post", backref='user', lazy=True)
 
     def __init__(self, full_name, username, password):
         self.full_name = full_name
