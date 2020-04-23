4b4f58af1eeb490ac027da32fcaf8ae0203f1133
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-04-24 00:36:39 +0300

    Deleting posts

--- application/posts/views.py
+++ application/posts/views.py
@@ -21,27 +21,42 @@ def posts_index():
 def posts_form():
     return render_template("posts/new.html", form = PostForm())
 
-@app.route("/posts/<post_id>/", methods=["POST"])
+@app.route("/posts/<post_id>/", methods=["GET", "POST"])
 @login_required
 def posts_vote(post_id):
-    p = Post.query.get(post_id)
+    if request.method == "POST":
+        p = Post.query.get(post_id)
 
-    # Check if user has already voted this post
-    stmt = text("SELECT id FROM vote WHERE user_id = :u_id AND post_id = :p_id").params(u_id=current_user.id, p_id=post_id)
-    res = db.engine.execute(stmt)
-    response = []
-    for row in res:
-        response.append(row)
-    # If so, don't let the user vote again.
-    if len(response) > 0:
-        return redirect(url_for("posts_index"))
+        # Check if user has already voted this post
+        stmt = text("SELECT id FROM vote WHERE user_id = :u_id AND post_id = :p_id").params(u_id=current_user.id, p_id=post_id)
+        res = db.engine.execute(stmt)
+        response = []
+        for row in res:
+            response.append(row)
+        # If so, don't let the user vote again.
+        if len(response) > 0:
+            return redirect(url_for("posts_index"))
 
-    # User hasn't voted yet, add a vote to 'vote' table
-    v = Vote(current_user.id, p.id)
-    db.session().add(v)
-    db.session().commit()
-
+        # User hasn't voted yet, add a vote to 'vote' table
+        v = Vote(current_user.id, p.id)
+        db.session().add(v)
+        db.session().commit()
+    elif request.method == "GET":
+        posts = Post.query.filter_by(id=post_id)
+        post_list = []
+        for post in posts:
+            post_list.append((post, User.query.get(post.user_id).full_name, Vote.get_post_vote_count(post.id)))
+        return render_template("/posts/list.html", posts = post_list)
     return redirect(url_for("posts_index"))
+
+@app.route("/posts/<post_id>/delete", methods=["POST"])
+@login_required
+def posts_delete(post_id):
+    post = Post.query.get(post_id)
+    if post.user_id == current_user.id:
+        db.session().delete(post)
+        db.session().commit()
+    return redirect(request.referrer)
 
 @app.route("/posts/", methods=["POST"])
 @login_required
